9fd720e0de58ade708207ec20c2497732e802d4c
Michel <michelmukua@michels-MacBook-Pro.local>
2025-02-27 02:50:20 -0700

    1996

---
+++ Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+   int year = 1996;
+   String month ="June";
+   boolean = false; 
+   system.out.println("1996");
+   system.out.println("June");
+   system.out.println("false");
+  }
+}
