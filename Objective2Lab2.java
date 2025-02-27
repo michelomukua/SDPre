74964f45f3811815053d255094e9672d1ae0b149
Michel <michelmukua@michels-MacBook-Pro.local>
2025-02-27 02:06:31 -0700

    Name: Michel Mukua Age: 50 State: Colorado

---
+++ Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Michel Mukua";
+    int age = 50;
+    String state = "Colorado";
+
+    system.out.println("Michel Mukua");
+    System.out.println("50");
+    System.out.println("Colorado");
+  
+  }
+}
