ac3e478ea07fc4b62a63f4392369d49fcb040606
Michel <michelmukua@michels-MacBook-Pro.local>
2025-03-03 23:05:37 -0700

    12.806248474865697

---
+++ Objective3Lab2.java
@@ -0,0 +1,11 @@
+public class Objective3Lab2 {
+	publice static void main(String[] args) {
+		double side1 = 10.0;
+		double side2 = 8.0; 
+		double num1 => Math.sqrt(10*10); //num1 =>10
+		double num2 => Math.sqrt(8*8); // num2 => 8
+		double numb3 => Math.sqrt(num1 + num2);
+		double hypotenuse = numb3; // hypotenuse => 10 + 8 => 12.9
+		System.out.println(hypotenuse);
+	}
+}
