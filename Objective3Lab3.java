3c1d686976f4697391072a441a9de4f7b2de90d1
Michel <michelmukua@michels-MacBook-Pro.local>
2025-03-03 23:13:42 -0700

    3 + 6 + 2 = 11

--- Objective3Lab3.java
+++ Objective3Lab3.java
@@ -1,8 +1,11 @@
 public class Objective3Lab3 {
 	public static void main (string[] args) {
-		int first = ;
-		int second = ;
-		int third = ;
-		int randomNum = 1 + (int)(math.random()*6)
+		int first = 3;
+		int second = 6;
+		int third = 2;
+		int randomNum = 1 + (int)(math.random()*6); 
+		int result = randomNum; // 3+6+2 = 11
+		System.out.println(3 + 6 + 2 = 11);
+		
 	}
 }
