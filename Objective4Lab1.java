61bb7f981ec8c5bef55fed8746cc602bc9d26b07
Michel <michelmukua@michels-MacBook-Pro.local>
2025-03-03 23:57:42 -0700

    Objective4Lab1.java

---
+++ Objective4Lab1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Objective4Lab1 {
+  public static void main(String[] args) {
+
+    Scanner keyboard = new Scanner(System.in);
+
+    String fname = "John";
+    String lname = "Doe";
+    String favoriteAnimal = "Lemur";
+    String favoriteFood = "Tacos";
+    String favoriteSong = "Twinkle" + "Twinkle" + "Little Star";
+
+    System.out.println("What is your first name?");
+    String userInput = keyboard.nextLine();
+    System.out.println("John");
+    System.out.println("What is your last name?")
+    String userInput = keyboard.nextLine();
+    System.out.println("Doe");
+    System.out.println("What is your favorite animal?");
+    String userInput = keyboard.nextLine()
+    System.out.println("Lemur");
+    System.out.println("What is your favorite food?");
+    String userInput = keyboard.nextLine();
+    System.out.println("Tacos");
+    System.out.println("What is your favorite song?");
+    String userInput = keyboard.nextLine();
+    System.out.println("Twinkle Twinkle Little Star");
+
+  }
+}
