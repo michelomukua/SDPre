5de1eebb98b20b102c8aa45aec78e40407a11b5d
Michel <michelmukua@michels-MacBook-Pro.local>
2025-03-03 22:02:04 -0700

    Objective3Lab1.java

---
+++ Objective3Lab1.java
@@ -0,0 +1,7 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1 = 15;
+    int num2 = 6;
+    int num3 = 2;
+    int num4 = 3;  
+    double average = (15+6+2)/3
